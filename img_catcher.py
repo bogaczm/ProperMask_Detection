c469092191bbae28b3b0d65833dc15df19b2e4e3
bogaczm <m.bogacz197@gmail.com>
2020-12-02 21:12:14 +0100

    img_collectro and labels

---
+++ img_catcher.py
@@ -0,0 +1,47 @@
+# Using openCV to get multile mask and no mask images from webcam
+import cv2
+# UUID i so everyone can create data without name collisions
+import uuid
+
+# Video stream
+wideo = cv2.VideoCapture(0)
+
+# Name generation
+IMG_PATH = './images/'
+
+
+# analysis loop
+while(wideo):
+    _, ramka = wideo.read()
+
+    cv2.imshow('Your Face',ramka)
+
+    # q - no mask
+    # w - wrong mask save
+    # e = good mask
+    # ESC - wyjscie z programu
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27 :
+        break
+
+    elif k == ord('q'):
+        name = 'NoMask_'+str(uuid.uuid1())
+        img_path = IMG_PATH+name+'.jpg'
+        cv2.imwrite(img_path, ramka)
+        print('Img saved as: {}'.format(name))
+
+    elif k == ord('w'):
+        name = 'WrongMask_'+str(uuid.uuid1())
+        img_path = IMG_PATH+name+'.jpg'
+        cv2.imwrite(img_path, ramka)
+        print('Img saved as: {}'.format(name))
+
+    elif k == ord('e'):
+        name = 'GoodMask_'+str(uuid.uuid1())
+        img_path = IMG_PATH+name+'.jpg'
+        cv2.imwrite(img_path, ramka)
+        print('Img saved as: {}'.format(name))
+    
+
+wideo.release()       
+cv2.destroyAllWindows()
